f46eebe41545e75df5c7a736be4d2a4262236751
tamil <tamil@HP>
2024-01-17 12:29:12 +0530

    Add project files.

---
+++ QRcode/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QRcode.Models;
+using QRCoder;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Net;
+using System.Net.Mail;
+using static System.Net.WebRequestMethods;
+
+namespace QRcode.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            string path= "https://www.shutterstock.com/image-illustration/anime-boy-watching-beautiful-sky-600nw-2327915433.jpg";
+            QRCodeGenerator QrGenerator = new QRCodeGenerator();
+            QRCodeData QrCodeInfo = QrGenerator.CreateQrCode(path, QRCodeGenerator.ECCLevel.Q);
+            QRCode QrCode = new QRCode(QrCodeInfo);
+            Bitmap QrBitmap = QrCode.GetGraphic(60);
+            byte[] BitmapArray = BitmapToByteArray(QrBitmap);
+            string QrUri = string.Format("data:image/png;base64,{0}", Convert.ToBase64String(BitmapArray));
+            ViewBag.QrCodeUri = QrUri;
+            return View();
+        }
+
+        private byte[] BitmapToByteArray(Bitmap qrBitmap)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                qrBitmap.Save(ms, ImageFormat.Png);
+                return ms.ToArray();
+            }
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
